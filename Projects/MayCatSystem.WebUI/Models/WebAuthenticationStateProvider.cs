8d82cb5d78c43f8d5b8049916209611754860ea6
tiephoangdev <30700664+TiepHoangDev@users.noreply.github.com>
2022-11-20 18:43:09 +0700

    add setting view + login

---
+++ Projects/MayCatSystem.WebUI/Models/WebAuthenticationStateProvider.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components;
+using Blazored.SessionStorage;
+
+namespace MayCatSystem.WebUI.Models
+{
+    public class WebAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public ISessionStorageService _sessionStorage { get; set; }
+
+        public WebAuthenticationStateProvider(ISessionStorageService sessionStorage)
+        {
+            _sessionStorage = sessionStorage;   
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var savedToken = await _sessionStorage.GetItemAsync<string>("username");
+
+            ClaimsIdentity ClaimsIdentity;
+            if (string.IsNullOrWhiteSpace(savedToken))
+            {
+                ClaimsIdentity = new ClaimsIdentity();
+            }
+            else
+            {
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name, savedToken)
+                };
+                ClaimsIdentity = new ClaimsIdentity(claims, "username");
+            }
+            return new AuthenticationState(new ClaimsPrincipal(ClaimsIdentity));
+        }
+    }
+}
---
+++ Projects/MayCatSystem.WebUI/Services/AccountService.cs
@@ -0,0 +1,28 @@
+﻿using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace MayCatSystem.WebUI.Services
+{
+    public class AccountService
+    {
+        public ISessionStorageService _sessionStorage { get; set; }
+        public AuthenticationStateProvider _authenticationStateProvider { get; set; }
+
+        public AccountService(ISessionStorageService sessionStorage, AuthenticationStateProvider authenticationStateProvider)
+        {
+            _sessionStorage = sessionStorage;
+            _authenticationStateProvider = authenticationStateProvider;
+        }
+
+        public async Task<bool> Login(string username, string password)
+        {
+            if (username == "admin" && password == "admin")
+            {
+                await _sessionStorage.SetItemAsync("username", username);
+                return true;
+            }
+            return false;
+        }
+    }
+}
